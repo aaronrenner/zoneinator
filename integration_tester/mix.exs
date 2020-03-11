2ef82c778f5e0cb87e5f236ab6dfd04a14351749
Aaron Renner <aaron.renner@gmail.com>
2020-03-11 09:00:56 -0600

    Use chromedriver for integration tests

--- integration_tester/mix.exs
+++ integration_tester/mix.exs
@@ -21,7 +21,7 @@ defmodule Zoneinator.IntegrationTester.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:wallaby, "~> 0.23.0", [runtime: false, only: :test]}
+      {:wallaby, github: "elixir-wallaby/wallaby", runtime: false, only: :test}
     ]
   end
 end
--- integration_tester/test/test_helper.exs
+++ integration_tester/test/test_helper.exs
@@ -1,6 +1,7 @@
 base_url = System.get_env("BASE_URL", "http://localhost:4000")
 
 Application.put_env(:wallaby, :base_url, base_url)
+Application.put_env(:wallaby, :driver, Wallaby.Experimental.Chrome)
 
 {:ok, _} = Application.ensure_all_started(:wallaby)
 
